607473fbe3adfa313f9e802574b086eb289423bf
Danila Panagushin <danildota1709@gmail.com>
2022-05-17 13:51:12 +0300

    Upload start files

---
+++ main.py
@@ -0,0 +1,13 @@
+with open('Electro-Myography-EMG-Dataset/raw_emg_data_unprocessed/index_finger_motion_raw.csv') as f:
+    data = []
+    for line in f:
+        data.append(list(map(float, line.strip().split(','))))
+
+
+for i in range(5):
+    print(data[i])
+
+print()
+for i in range(-5, 0, 1):
+    print(data[i])
+
